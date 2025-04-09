ac7d6f568e500af8dbabc55662756021d36cf443
Leonardo Castro <carlosleocastro1984@gmail.com>
2025-04-09 13:57:19 -0300

    Reemplazo archivos Excel por CSV para compatibilidad con Streamlit Cloud

--- app/07_app_visualizacion.py
+++ app/07_app_visualizacion.py
@@ -724,8 +724,8 @@ elif seccion == "Similares":
     st.markdown("Encuentra jugadores con perfiles estadísticos similares durante la Copa América 2024.")
 
     # Carga de archivos externos
-    df_datos = pd.read_excel("Data/eventos_copa_america/Copa_America_24.xlsx")
-    df_metricas = pd.read_excel("Data/eventos_copa_america/Metricas.xlsx")
+    df_datos = pd.read_excel("Proyecto Final/Data/eventos_copa_america/Copa_America_24.csv")
+    df_metricas = pd.read_excel("Proyecto Final/Data/eventos_copa_america/Metricas.csv")
 
     # Limpiar nombres de columnas (muy importante)
     df_datos.columns = df_datos.columns.str.strip()
@@ -834,7 +834,7 @@ elif seccion == "Agrupamientos":
     st.markdown("Esta visualización agrupa a los jugadores en 3 clusters según sus métricas durante la Copa América 2024.")
 
     # Cargar archivo base
-    df = pd.read_excel("Data/eventos_copa_america/Copa_America_24.xlsx")
+    df = pd.read_excel("Proyecto Final/Data/eventos_copa_america/Copa_America_24.csv")
     df.columns = df.columns.str.strip()
 
     # Filtro por minutos
@@ -856,7 +856,7 @@ elif seccion == "Agrupamientos":
     }
 
     # Cargar métricas por posición
-    metricas_pos = pd.read_excel("Data/eventos_copa_america/Metricas.xlsx")
+    metricas_pos = pd.read_excel("Proyecto Final/Data/eventos_copa_america/Metricas.csv")
     
     # Seleccionar posición del usuario
     pos_sel = st.selectbox("Selecciona una posición", list(posiciones.keys()))
